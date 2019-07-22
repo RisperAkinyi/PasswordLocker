dc3189ee26f7f50aea70fc3f8df5fb3682fb9512
Risper Akinyi <email@example.com>
2019-07-22 10:09:24 +0300

    Add class User

---
+++ User.py
@@ -0,0 +1,10 @@
+class User
+    """
+    Class that generates new instance
+    """
+    user_list = [] #Empty user list
+    
+    def __init__(self,idNo,username,password):
+        self.id = idNo
+        self.user = username
+        self.psw = password
