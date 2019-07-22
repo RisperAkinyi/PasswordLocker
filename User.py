45a5acf6e7faf3c0e6ee691213194cabd6faee81
Risper Akinyi <email@example.com>
2019-07-22 10:25:45 +0300

    import unittest to userTest.py

--- User.py
+++ User.py
@@ -1,10 +1,24 @@
-class User
+class User:
     """
     Class that generates new instance
     """
     user_list = [] #Empty user list
     
-    def __init__(self,idNo,username,password):
-        self.id = idNo
-        self.user = username
-        self.psw = password
+    def __init__(self, first_name, last_name, username, password):
+        """
+        Defines properties for our objects.
+
+        Args:
+            first_name: New user first name.
+            last_name: New user last name.
+            username: New user username.
+            password: New user password.
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+
+        
+
